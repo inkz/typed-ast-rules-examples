0a8d471096834db0931504ba58cda5d263fd9b07
inkz <inkz@xakep.ru>
2019-08-30 18:03:16 +1000

    Update require-request examples

--- require-request-koa/index.js
+++ require-request-koa/index.js
@@ -2,58 +2,99 @@ const Koa = require('koa');
 const app = new Koa();
 const port = 3002
 
+// false
 app.use(async ctx => {
-  const val = ctx.request.query.val
-  const data = require(val)
-  ctx.body = {data}
+  ctx.body = 'Hello World!'
 })
 
+// false
 app.use(async ctx => {
-  const val = ctx.request.body.val
-  const data = require(val)
-  ctx.body = {data}
+  const val22 = 'smth'
+  const data22 = require(val22)
+  ctx.body = 'ok'
 })
 
+// false
 app.use(async ctx => {
-  const val = ctx.request.get('val')
-  const data = require(val)
-  ctx.body = {data}
+  const data33 = ctx.request.body.val33
+  ctx.body = 'ok' + data33
 })
 
+// false
 app.use(async ctx => {
-  const val = ctx.get('val')
-  const data = require(val)
-  ctx.body = {data}
+  const data44 = ctx.request.body.val44
+  const obj44 = `smth ${data44}`
+  ctx.body = 'ok' + obj44
 })
 
+// false
 app.use(async ctx => {
-  const val = ctx.request.querystring
-  const data = require(val)
-  ctx.body = {data}
+  const data55 = ctx.request.query.val55
+  const obj55 = 'smth' + data55
+  ctx.body = 'ok' + obj55
 })
 
+// true
 app.use(async ctx => {
-  const val = ctx.request.href
-  const data = require(val)
-  ctx.body = {data}
+  const val1 = ctx.request.query.val1
+  ctx.body = {data: {
+    name: require(val1)
+  }}
 })
 
+// true
 app.use(async ctx => {
-  const val = ctx.href
-  const data = require(val)
-  ctx.body = {data}
+  const val2 = ctx.request.body.val2
+  const data2 = require(val2)
+  ctx.body = `aaa ${data2}`
 })
 
+// true
 app.use(async ctx => {
-  const val = ctx.query.val
-  const data = require(val)
-  ctx.body = {data}
+  const val3 = ctx.request.get('val')
+  const data3 = require(val3)
+  ctx.body = {data3}
 })
 
+// true
 app.use(async ctx => {
-  const val = ctx.querystring
-  const data = require(val)
-  ctx.body = {data}
+  const val4 = ctx.get('val')
+  const data4 = require(val4)
+  ctx.body = {data4}
+})
+
+// true
+app.use(async ctx => {
+  const val5 = ctx.request.querystring
+  const data5 = require(val5)
+  ctx.body = {data5}
+})
+
+// true
+app.use(async ctx => {
+  const val6 = ctx.request.href
+  const data6 = require('yo' + val6)
+  ctx.body = {data6}
+})
+
+// true
+app.use(async ctx => {
+  const val7 = ctx.href
+  const data7 = require(`asd ${val7}`)
+  ctx.body = {data7}
+})
+
+// true
+app.use(async ctx => {
+  const data8 = require(`zxc ${ctx.query.val}`)
+  ctx.body = {data8}
+})
+
+// true
+app.use(async ctx => {
+  const val9 = 
+  const data9 = require('vbn' + ctx.querystring)
+  ctx.body = {data9}
 })
 
 app.listen(port)
--- require-request/index.js
+++ require-request/index.js
@@ -9,62 +9,62 @@ app.get('/test1', (req, res) => {
 
 // false
 app.get('/test2', () => {
-  const val = 'smth'
-  const data = require(val)
+  const val2 = 'smth'
+  const data2 = require(val2)
   res.send('ok')
 })
 
 // false
 app.get('/test3', (req, res) => {
-  const data = req.body.val
-  res.send('ok' + data)
+  const data3 = req.body.val3
+  res.send('ok' + data3)
 })
 
 // false
 app.get('/test4', (req, res) => {
-  const data = req.body.val
-  const obj = `smth ${data}`
-  res.send('ok' + obj)
+  const data4 = req.body.val4
+  const obj4 = `smth ${data4}`
+  res.send('ok' + obj4)
 })
 
 // false
 app.get('/test5', (req, res) => {
-  const data = req.body.val
-  const obj = 'smth' + data
-  res.send('ok' + obj)
+  const data5 = req.body.val5
+  const obj5 = 'smth' + data5
+  res.send('ok' + obj5)
 })
 
 // true
 app.get('/test6', (req, res) => {
-  const data = require(req.query.page)
+  const data6 = require(req.query.page6)
   res.send('ok')
 })
 
 // true
 app.get('/test7', (req, res) => {
-  const data = req.query.page
-  const obj = require(data)
+  const data7 = req.query.page7
+  const obj = require(data7)
   res.send('ok')
 })
 
 // true
 app.get('/test8', (req, res) => {
-  const data = req.query.page
-  const obj = require('smth' + data)
+  const data8 = req.query.page8
+  const obj8 = require('smth' + data8)
   res.send('ok')
 })
 
 // true
 app.get('/test9', (req, res) => {
-  const data = req.query.page
-  const asd = require(`smth ${data}`)
+  const data9 = req.query.page9
+  const asd9 = require(`smth ${data9}`)
   res.send('ok')
 })
 
 // true
 app.get('/test10', (req, res) => {
-  const data = req.query.page
-  const obj = require('smth' + data)
+  const data10 = req.query.page10
+  const obj10 = require('smth' + data10)
   res.send('ok')
 })
 
