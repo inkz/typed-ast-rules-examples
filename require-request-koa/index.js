e69b2cd819f3ee6441558cf0021bf1554dd67836
inkz <inkz@xakep.ru>
2019-08-30 17:20:02 +1000

    Update koa examples

--- require-request-koa/index.js
+++ require-request-koa/index.js
@@ -8,4 +8,52 @@ app.use(async ctx => {
   ctx.body = {data}
 })
 
+app.use(async ctx => {
+  const val = ctx.request.body.val
+  const data = require(val)
+  ctx.body = {data}
+})
+
+app.use(async ctx => {
+  const val = ctx.request.get('val')
+  const data = require(val)
+  ctx.body = {data}
+})
+
+app.use(async ctx => {
+  const val = ctx.get('val')
+  const data = require(val)
+  ctx.body = {data}
+})
+
+app.use(async ctx => {
+  const val = ctx.request.querystring
+  const data = require(val)
+  ctx.body = {data}
+})
+
+app.use(async ctx => {
+  const val = ctx.request.href
+  const data = require(val)
+  ctx.body = {data}
+})
+
+app.use(async ctx => {
+  const val = ctx.href
+  const data = require(val)
+  ctx.body = {data}
+})
+
+app.use(async ctx => {
+  const val = ctx.query.val
+  const data = require(val)
+  ctx.body = {data}
+})
+
+app.use(async ctx => {
+  const val = ctx.querystring
+  const data = require(val)
+  ctx.body = {data}
+})
+
 app.listen(port)
