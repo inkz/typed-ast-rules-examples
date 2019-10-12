b97a013b8770de3bdd6f3aba407c49fa057c154f
inkz <inkz@xakep.ru>
2019-10-12 15:46:22 +1000

    Edit jwt-payload example

--- jwt-payload/index.js
+++ jwt-payload/index.js
@@ -19,3 +19,14 @@ const token3 = JWS.sign(payload, JWK.asKey(secret))
 const token4 = jsonwt.sign({one: 'two'}, secret)
 const token5 = JWT.sign({hello: 'world'}, JWK.asKey(secret))
 const token6 = JWS.sign({check: 'check'}, JWK.asKey(secret))
+
+const algo = 'HS256';
+const verified = jsonwt.verify(token1, secret, {algorithm: algo})
+
+const signOpts = {algorithm: 'HS256'}
+function verify(opts) {
+  const verified = jsonwt.verify(token1, secret, opts)
+  return verified
+}
+const verified2 = verify(signOpts)
+const decoded = jsonwt.decode(token1)
